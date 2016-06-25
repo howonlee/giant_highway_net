06135a8f7b60e6c598cd21c16e1fb44b7f6330a8
Howon Lee <howon@howonlee.com>
2016-06-25 09:33:10 -0700

    thinking

--- mlp.py
+++ mlp.py
@@ -1,4 +1,27 @@
 import tensorflow as tf
 
+def create_mlp(size):
+    # h1 = tf.Variable(something)
+    # h2 = tf.Variable(something)
+    # out = tf.Variable(something)
+
+    # b1 = tf.Variable(something)
+    # b2 = tf.Variable(something)
+    # bout = tf.Variable(something)
+    return size
+
+def create_loss(mlp):
+    pass
+
+def create_opt(loss):
+    pass
+
 if __name__ == "__main__":
-    print "hi"
+    for size in xrange(50, 1000, 50):
+        curr_mlp = create_mlp(size)
+        curr_loss = create_loss(curr_mlp)
+        curr_opt = create_opt(curr_loss)
+        init = tf.initialize_all_variables()
+        with tf.Session() as sess:
+            sess.run(init)
+            print "woobs"
--- readme_qs.md
+++ readme_qs.md
@@ -1,6 +1,6 @@
 What are highway nets? How are they related to LSTM? What does gating do?
 
-Why make them huge? What does extensivity mean here (did you make that bullshit up?)
+Why make them huge? What does extensivity mean here (did you make that bullshit up? yes you did)? talk about criticality.
 
 What results did you get on a cool kids dataset (CIFAR-100)? Accuracy (that conforms to a cool kids paper on CIFAR, I guess)?
 
