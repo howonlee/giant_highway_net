7d6bcb255b4181a625dd6c060b35085ebcb359c6
Howon Lee <howon@howonlee.com>
2016-06-27 23:16:25 -0700

    all well and good with the mlp

--- mlp.py
+++ mlp.py
@@ -5,8 +5,8 @@ import gzip
 import datetime
 
 params = {
-        "learning_rate": 0.001,
-        "training_epochs": 15,
+        "learning_rate": 0.005,
+        "training_epochs": 7,
         "batch_size": 100,
         "display_step": 1
         }
@@ -16,19 +16,23 @@ net_params = {
         "n_classes": 10
         }
 
-def create_mlp(size, curr_x):
+def create_mlp(size, curr_x, keep_prob):
     # make vars
-    h1 = tf.Variable(tf.random_normal([net_params["n_input"], size]))
-    b1 = tf.Variable(tf.random_normal([size]))
-    # h2 = tf.Variable(something) ????
-    # b2 = tf.Variable(something) ???
-    out = tf.Variable(tf.random_normal([size, net_params["n_classes"]]))
-    bout = tf.Variable(tf.random_normal([net_params["n_classes"]]))
+    h1 = tf.Variable(tf.truncated_normal([net_params["n_input"], size], stddev=0.1))
+    b1 = tf.Variable(tf.constant(0.1, shape=[size]))
+    h2 = tf.Variable(tf.truncated_normal([size, size], stddev=0.1))
+    b2 = tf.Variable(tf.constant(0.1, shape=[size]))
+    out = tf.Variable(tf.truncated_normal([size, net_params["n_classes"]], stddev=0.1))
+    bout = tf.Variable(tf.constant(0.1, shape=[net_params["n_classes"]]))
 
     # make layers
     layer_1 = tf.add(tf.matmul(curr_x, h1), b1)
+    layer_1 = tf.nn.dropout(layer_1, keep_prob)
     layer_1 = tf.nn.relu(layer_1)
-    out_layer = tf.add(tf.matmul(layer_1, out), bout)
+    layer_2 = tf.add(tf.matmul(layer_1, h2), b2)
+    layer_2 = tf.nn.dropout(layer_2, keep_prob)
+    layer_2 = tf.nn.relu(layer_2)
+    out_layer = tf.add(tf.matmul(layer_2, out), bout)
     return out_layer
 
 def create_loss(mlp, y):
@@ -46,10 +50,11 @@ def onehotify(batch):
 if __name__ == "__main__":
     with gzip.open("mnist.pkl.gz") as mnist_file:
         train, valid, test = cPickle.load(mnist_file)
-    for size in xrange(50, 1000, 50):
+    for size in xrange(50, 2500, 50):
         curr_x = tf.placeholder("float", [None, net_params["n_input"]])
         curr_y = tf.placeholder("float", [None, net_params["n_classes"]])
-        curr_mlp = create_mlp(size, curr_x)
+        keep_prob = tf.placeholder("float")
+        curr_mlp = create_mlp(size, curr_x, keep_prob)
         curr_loss = create_loss(curr_mlp, curr_y)
         curr_opt = create_opt(curr_loss)
         init = tf.initialize_all_variables()
@@ -68,7 +73,7 @@ if __name__ == "__main__":
                     batch_x, batch_y =\
                             train[0][batch_start:batch_start+params["batch_size"]],\
                             onehotify(train[1][batch_start:batch_start+params["batch_size"]])
-                    curr_feed_dict = {curr_x: batch_x, curr_y: batch_y}
+                    curr_feed_dict = {keep_prob:0.5, curr_x: batch_x, curr_y: batch_y}
                     _, c = sess.run([curr_opt, curr_loss], feed_dict=curr_feed_dict)
                     avg_cost += c / total_batch
                 # if epoch % params["display_step"] == 0:
@@ -77,4 +82,4 @@ if __name__ == "__main__":
             print "opt finished"
             corrects = tf.equal(tf.argmax(curr_mlp, 1), tf.argmax(curr_y, 1))
             acc = tf.reduce_mean(tf.cast(corrects, "float"))
-            print "acc: ", acc.eval({curr_x: test[0], curr_y: onehotify(test[1])})
+            print "acc: ", acc.eval({keep_prob:1.0, curr_x: test[0], curr_y: onehotify(test[1])})
