3467797da13dcec0c921685246b510c372554b17
Howon Lee <howon@howonlee.com>
2016-06-28 00:19:12 -0700

    there is no extensivity

--- mlp.py
+++ mlp.py
@@ -1,28 +1,32 @@
 import tensorflow as tf
 import numpy as np
+import math
 import cPickle
 import gzip
 import datetime
 
 params = {
-        "learning_rate": 0.005,
-        "training_epochs": 7,
-        "batch_size": 100,
+        "learning_rate": 0.05,
+        "training_epochs": 1,
+        "batch_size": 200,
         "display_step": 1
         }
 
 net_params = {
-        "n_input": 784, # MNIST, iow
-        "n_classes": 10
+        "n_input": 3072, # CIFAR-100, iow
+        "n_classes": 100
         }
 
 def create_mlp(size, curr_x, keep_prob):
     # make vars
-    h1 = tf.Variable(tf.truncated_normal([net_params["n_input"], size], stddev=0.1))
+    h1_stddev = math.sqrt(3.0 / (net_params["n_input"] + size))
+    h1 = tf.Variable(tf.truncated_normal([net_params["n_input"], size], stddev=h1_stddev))
     b1 = tf.Variable(tf.constant(0.1, shape=[size]))
-    h2 = tf.Variable(tf.truncated_normal([size, size], stddev=0.1))
+    h2_stddev = math.sqrt(3.0 / (size + size))
+    h2 = tf.Variable(tf.truncated_normal([size, size], stddev=h2_stddev))
     b2 = tf.Variable(tf.constant(0.1, shape=[size]))
-    out = tf.Variable(tf.truncated_normal([size, net_params["n_classes"]], stddev=0.1))
+    out_stddev = math.sqrt(3.0 / (size + net_params["n_classes"]))
+    out = tf.Variable(tf.truncated_normal([size, net_params["n_classes"]], stddev=out_stddev))
     bout = tf.Variable(tf.constant(0.1, shape=[net_params["n_classes"]]))
 
     # make layers
@@ -48,9 +52,13 @@ def onehotify(batch):
     return new_batch
 
 if __name__ == "__main__":
-    with gzip.open("mnist.pkl.gz") as mnist_file:
-        train, valid, test = cPickle.load(mnist_file)
-    for size in xrange(50, 2500, 50):
+    with open("cifar-100-python/train", "rb") as train_file:
+        train_dict = cPickle.load(train_file)
+        train = [train_dict['data'] / 255.0, np.array(train_dict['fine_labels'])]
+    with open("cifar-100-python/test", "rb") as test_file:
+        test_dict = cPickle.load(test_file)
+        test = [test_dict['data'] / 255.0, np.array(test_dict['fine_labels'])]
+    for size in xrange(1300, 2500, 500):
         curr_x = tf.placeholder("float", [None, net_params["n_input"]])
         curr_y = tf.placeholder("float", [None, net_params["n_classes"]])
         keep_prob = tf.placeholder("float")
@@ -68,6 +76,8 @@ if __name__ == "__main__":
                 total_batch = int(len(train[0])/params["batch_size"])
                 # print "total batch", total_batch
                 for i in range(total_batch):
+                    if i % 5 == 0:
+                        print i, " / ", total_batch, datetime.datetime.now()
                     batch_start = i * params["batch_size"]
                     # ugly, deal with it
                     batch_x, batch_y =\
@@ -76,9 +86,6 @@ if __name__ == "__main__":
                     curr_feed_dict = {keep_prob:0.5, curr_x: batch_x, curr_y: batch_y}
                     _, c = sess.run([curr_opt, curr_loss], feed_dict=curr_feed_dict)
                     avg_cost += c / total_batch
-                # if epoch % params["display_step"] == 0:
-                #     print "Epoch:", '%04d' % (epoch+1), "cost=", \
-                #         "{:.9f}".format(avg_cost)
             print "opt finished"
             corrects = tf.equal(tf.argmax(curr_mlp, 1), tf.argmax(curr_y, 1))
             acc = tf.reduce_mean(tf.cast(corrects, "float"))
